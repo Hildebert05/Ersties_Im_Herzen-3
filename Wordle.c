c9fbf1ca77f592151dc2e393417a16a306771366
chr5109 <jonas.uecker@tuhh.de>
2025-11-29 16:50:12 +0100

    hab die funktion selectWord hinzugefügt

--- Wordle.c
+++ Wordle.c
@@ -3,17 +3,30 @@
 #include <stdlib.h>
 
 
+#define TRY 5
 
 
+void selectWord(char **array, int size){
+    int randomWord = rand() % size;
+    printf("%s\n", array[randomWord]);
+}
 
-
+void guessWord(){
+    for(int i = 0; i < TRY; i++){
+        
+    }
+}
 
 
 int main(void){
 
-    char Words[] = {};
+    srand(time(NULL));
 
 
+    char *words[] = {"Hallo", "Blume", "Taste", "Buche"};
+    int wordCnt = sizeof(words) / sizeof(words[0]);
+   
+    selectWord(words, wordCnt);
 
     return 0;
 }
